66304ba0b005680aacd446e56ae825724f8ab397
yamividaurre <yamividaurre@gmail.com>
2022-09-28 14:47:34 -0300

    creacion

---
+++ a.js
@@ -0,0 +1,8 @@
+let n1 = parseInt(prompt('ingrese el primer valor'))
+let n2 = parseInt(prompt('ingrese el segundo valor'))
+let n3 = parseInt(prompt('ingrese el tercer valor'))
+let n4 = parseInt(prompt('ingrese el cuarto valor'))
+let n5 = parseInt(prompt('ingrese el ultimo valor'))
+
+let r = n1 + n2 + n3 + n4 + n5;
+alert(r);
