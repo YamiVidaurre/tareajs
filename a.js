ca3297e50067d04823d9fedc6ad6fe49eb2b561c
yamividaurre <yamividaurre@gmail.com>
2022-09-29 21:29:37 -0300

    modificacion de alert

--- a.js
+++ a.js
@@ -1,8 +1,8 @@
-let n1 = parseInt(prompt('ingrese el primer valor'))
-let n2 = parseInt(prompt('ingrese el segundo valor'))
-let n3 = parseInt(prompt('ingrese el tercer valor'))
-let n4 = parseInt(prompt('ingrese el cuarto valor'))
-let n5 = parseInt(prompt('ingrese el ultimo valor'))
+let n1 = parseInt(prompt('Ingrese el primer valor'))
+let n2 = parseInt(prompt('Ingrese el segundo valor'))
+let n3 = parseInt(prompt('Ingrese el tercer valor'))
+let n4 = parseInt(prompt('Ingrese el cuarto valor'))
+let n5 = parseInt(prompt('Ingrese el ultimo valor'))
 
 let r = n1 + n2 + n3 + n4 + n5;
-alert(r);
+alert(`La suma de ${n1} + ${n2} + ${n3} + ${n4} + ${n5} es ${r}`)
